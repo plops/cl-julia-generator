64ee26f75628ded8a0def83b9b8156c47051ab48
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-01-08 11:03:46 +0100

    look into uuid

---
+++ example/02_pluto_notebook/gen00.lisp
@@ -0,0 +1,89 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-julia-generator")
+  (ql:quickload "alexandria"))
+(in-package :cl-julia-generator)
+
+
+
+(progn
+  (defparameter *path* "/home/martin/stage/cl-julia-generator/example/01_adfem")
+  (defparameter *code-file* "run_00_adfem")
+  (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
+
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+
+     
+  (let* (
+	 
+	 (code
+	  `(do0
+	    (do0
+	     "using Pkg"
+	     (Pkg.add (string "ADCME"))
+	     (Pkg.build (string "ADCME"))
+	     (Pkg.add (string "AdFem"))
+	     (Pkg.build (string "AdFem"))
+	     (Pkg.add (string "PyPlot"))
+	     (Pkg.build (string "PyPlot"))
+	     
+	     ))))
+    (write-source (format nil "~a/source/~a" *path* *code-file*) code)
+    (write-source
+     (format nil "~a/source/~a" *path* "run_01_poisson")
+     `(do0
+       "using AdFem"
+       "using PyPlot"
+
+       (setf mesh (Mesh (joinpath PDATA (string "twoholes_large.stl")))
+	     xy (gauss_nodes mmesh)
+	     k (@.
+		(+ (* (sin (aref xy ":" 1))
+		      (^ (aref xy ":" 2) 2))
+		   1.0))
+	     f (* 1e5 (@. (+ (aref xy ":" 1)
+			     (aref xy ":" 2))))
+	     K (compute_fem_laplace_matrix k mmesh)
+	     F (compute_fem_source_term1 f mmesh)
+	     bdnode (bcnode mmesh)
+	     (ntuple K F) (impose_Dirichlet_boundary_conditions K F bdnode
+								(zeros (length bdnode)))
+	     sol (backslash K F))
+       (do0
+	(setf nn_k)))
+     )
+    (write-source
+     (format nil "~a/source/~a" *path* "run_02_adcme")     `(do0
+       "using LinearAlgebra"
+       "using ADCME"
+       (setf n 101
+	     h (/ 1 (- n 1))
+	     x (aref (LinRange 0 1 n)
+		     (slice 2 (- end 1)))
+	     b (Variable 10.0)
+	     A (diagm (=> 0 (* (/ 2 (^ h 2)
+				  )
+			       (ones (- n 2))))
+		      (=> -1 (* (/ -1 (^ h 2)
+					)
+				(ones (- n 3))))
+		      (=> 1 (* (/ -1 (* (^ h 2)
+					))
+			       (ones (- n 3)))))
+	     B (+ (* b A) I)
+	     f (@. (* 4 (+ 2 x (* -1 (^ x 2)))))
+	     u (backslash B f)
+	     ue (aref u (div (+ n 1)
+			     2))
+	     loss (^ (- ue 1.0) 2)
+	     sess (Session))
+       (init sess)
+       (BFGS! sess loss)
+       (println (string "estimated b=")
+		(run sess b)))
+     )))
+
+
+
