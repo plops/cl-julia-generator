33174b7f5ee597b7b03d3d81e238644c7f8dd950
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-01-08 10:25:03 +0100

    start working on notebook generation

--- jl.lisp
+++ jl.lisp
@@ -6,6 +6,51 @@
 
 (defparameter *file-hashes* (make-hash-table))
 
+(defun write-notebook (&key nb-file nb-code)
+  "write julia jupyter notebook"
+  (let ((tmp (format nil "~a.tmp" nb-file)))
+    (with-output-to-file (s tmp :if-exists :supersede
+				:if-does-not-exist :create)
+      (format s "~a~%"
+	      (jonathan:to-json
+	       `(:|cells|
+		  ,(loop for e in nb-code
+			 collect
+			 (destructuring-bind (name &rest rest) e
+			   (case name
+			     (`markdown `(:cell_type "markdown"
+					  :metadata :empty
+					  :source
+					  ,(loop for p in rest
+						 collect
+						 (format nil "~a~c" p #\Newline))))
+			     (`python `(:cell_type "code"
+					:metadata :empty
+					:execution_count :null
+					:outputs ()
+					:source
+					,(loop for p in rest
+					       appending
+					       (let ((tempfn "/dev/shm/cell"))
+						 (write-source tempfn p)
+						 (with-open-file (stream (format nil "~a.py" tempfn))
+						   (loop for line = (read-line stream nil)
+							 while line
+							 collect
+							 (format nil "~a~c" line #\Newline)))))))
+			     )))
+		  :|metadata| (:|kernelspec| (:|display_name| "Python 3"
+					       :|language| "python"
+					      :|name| "python3"))
+		 :|nbformat| 4
+		  :|nbformat_minor| 2))))
+    #+nil
+    (sb-ext:run-program "/usr/bin/python3" `("-mjson.tool" ,nb-file))
+    (sb-ext:run-program "/usr/bin/jq" `("-M" "." ,tmp)
+			:output nb-file
+			:if-output-exists :supersede)
+    (delete-file tmp)))
+
 (defun write-source (name code &optional (dir (user-homedir-pathname))
 				 ignore-hash)
   (let* ((fn (merge-pathnames (format nil "~a.jl" name)
