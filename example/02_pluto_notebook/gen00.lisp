b8ee838940736e019464fd980ab3da630f6252df
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-01-08 11:22:36 +0100

    still complaining
    syntax: invalid syntax (incomplete #<julia: "incomplete: premature end of input">)
    
        top-level scope@none:1

--- example/02_pluto_notebook/gen00.lisp
+++ example/02_pluto_notebook/gen00.lisp
@@ -34,6 +34,10 @@
 	  (setf a 2))
      (do0 (comments "╔═╡ 99f6fe7c-37dd-49a9-b0ba-af87eb3f7bab")
 	  (setf b (+ a 3)))
+     (do0
+      (comments "╔═╡ Cell order:"
+		"╠═45ce7b9c-7066-11ec-29b0-13acfccc64a9"
+		"╠═99f6fe7c-37dd-49a9-b0ba-af87eb3f7bab"))
      )
    )
   )
